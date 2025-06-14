27129ac4e7342922110bf4d6b3672322d41c2c70
DapCodes <daffaramadhan929@gmail.com>
2025-06-14 12:39:13 +0700

    finish task session 12. make component

---
+++ laravel-daffa/resources/views/components/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ url('/') }}">TokoKu</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="{{ url('/products') }}">Produk</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url('/cart') }}">Keranjang</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
